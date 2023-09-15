ff958fc1bcaf4f357fb8a43f45192bea7974c07f
Brent Hoover <brent@thebuddhalodge.com>
2023-09-15 15:16:27 +0800

    feat: some housekeeping stuff

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import Core from "../packages/core/src/core.ts";
 import importPluginsJSONFile from "../packages/core/src/importPluginsJSONFile.ts";
 import log from "../packages/logger/index.ts";
 
-async function runApp() {
-    log("Starting bun-commerce");
-    await importPluginsJSONFile("../../../plugins.json")
+async function runApp () {
+  log("Starting bun-commerce");
+  await importPluginsJSONFile("../../../plugins.json");
 }
 
 runApp();
